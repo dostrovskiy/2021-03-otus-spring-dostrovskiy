88da5eebd26b440c301b88cc7c061b4be41ca2fd
Дмитрий Островский <gyimi@yandex.ru>
2021-04-28 11:54:49 +0300

    Добавил родителя для собственных исключений

--- mybooks/src/main/java/ru/otus/mybooks/exception/BookServiceRemoveBookException.java
+++ mybooks/src/main/java/ru/otus/mybooks/exception/BookServiceRemoveBookException.java
@@ -1,6 +1,6 @@
 package ru.otus.mybooks.exception;
 
-public class BookServiceRemoveBookException extends RuntimeException {
+public class BookServiceRemoveBookException extends MyBookAppException {
     private static final long serialVersionUID = -4689176991544730063L;
 
     public BookServiceRemoveBookException(String message) {
