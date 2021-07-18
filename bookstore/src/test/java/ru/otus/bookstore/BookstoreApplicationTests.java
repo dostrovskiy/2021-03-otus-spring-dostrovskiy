ebf0893c6cc14f1f57f93c4b269e8ded82dd59bd
Дмитрий Островский <gyimi@mail.ru>
2021-07-18 10:32:29 +0300

    Добавлены юнит-тесты

---
+++ bookstore/src/test/java/ru/otus/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
