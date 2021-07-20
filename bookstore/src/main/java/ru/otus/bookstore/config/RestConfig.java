4c27dee6bab640b14752649a9dc7e48cf35512a1
Дмитрий Островский <gyimi@mail.ru>
2021-07-20 14:18:31 +0300

    Добавлены файлы докера для сервисов mybooks и bookstore, а также docker-compose. Оба образа выложены на hub.docker.com в пространстве gyimix

--- bookstore/src/main/java/ru/otus/bookstore/config/RestConfig.java
+++ bookstore/src/main/java/ru/otus/bookstore/config/RestConfig.java
@@ -1,14 +1,26 @@
 package ru.otus.bookstore.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestConfig {
+    @Value("${mybooks.rest-template.connection-timeout}")
+    private int connectTimeout;
+    @Value("${mybooks.rest-template.read-timeout}")
+    private int readTimeout;
+
 
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofMillis(connectTimeout))
+                .setReadTimeout(Duration.ofMillis(readTimeout))
+                .build();
     }
 }
